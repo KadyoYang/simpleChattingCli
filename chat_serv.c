f20f69e7b41bdd0048aa675cdf37c296d213aba7
mrriey00 <mrriey00@naver.com>
2019-05-30 18:45:32 +0900

    설계중

--- chat_serv.c
+++ chat_serv.c
@@ -9,7 +9,7 @@
 
 
 
-
+#define MAX_USER_NUM 30
 #define BUF_SIZE 50
 
 #define EXIT_ROOM_CODE /exit
@@ -22,15 +22,20 @@
 struct sd_nickname_t{
 	int sd;
 	char* nickname;
-	int room;
-	int user_num;
+	int room; // 0 is room list i > 0 is specific room
+	int isuse; // 0 false 1 true
 };
 
 // 클라이언트 에서는 먼저 ./chat_clnt 127.0.0.1 9849 id 식으로 접속을 해야함 
 // 그 정보가 내가 손수만든 구조체에 들어가서 포인터로 날아간다
 void* clnt_thread_main(void* arg);
 void error_handler(char *message);
-struct sd_nickname_t user_info[30]; 
+int get_unused_num();
+
+struct sd_nickname_t user_info[MAX_USER_NUM]; 
+
+
+
 
 int main(int argc, char* argv[]){
 	int serv_sd, clnt_sd; //서버 클라이어느 소켓 디스크립터 변수 선언
@@ -40,13 +45,12 @@ int main(int argc, char* argv[]){
 	char nickname[BUF_SIZE];
 	int strlen = 0;
 	pthread_t thread_id;
+	int arraytemp;
 	
 	if(argc != 2){
 		printf("Usage : %s <port> \n", argv[0]);
 		exit(1);
-	}
-
-	
+	}	
 	serv_sd = socket(PF_INET, SOCK_STREAM, 0);
 	if(serv_sd == -1)
 		error_handler("socket() error");
@@ -71,7 +75,10 @@ int main(int argc, char* argv[]){
 		sd_nickname.sd = clnt_sd;
 		sd_nickname.nickname = &buf[0];	
 		sd_nickname.room = 0;
-		if((pthread_create(&thread_id, NULL, clnt_thread_main, (void*)&sd_nickname)) != 0){
+		sd_nickname.isuse = 1;
+		arraytemp = get_unused_num();
+		user_info[arraytemp] = sd_nickname; // ##########임게영역 
+		if((pthread_create(&thread_id, NULL, clnt_thread_main, (void*)&arraytemp)) != 0){
 			close(clnt_sd);
 			continue;
 		}
@@ -85,7 +92,7 @@ int main(int argc, char* argv[]){
 
 
 void* clnt_thread_main(void* arg){//arg로 그냥 몇번째 유저인지 번호만 알려주고 그 번호로 전역구조체 참조해서use
-	struct sd_nickname_t sd_nickname = *((sd_nickname*)arg);
+	int arraynum = *((int*)arg);
 	// 흠.. 
 	
 
@@ -103,3 +110,14 @@ void error_handler(char *message){
 	exit(1);
 }
 	
+int get_unused_num(){
+	int un_used_num;	
+	for(int i = 0; i < MAX_USER_NUM; i ++){
+		if(user_info[i].isuse == 0){
+			un_used_num = i;
+			break;
+		}		
+	}
+	return un_used_num;
+}
+
